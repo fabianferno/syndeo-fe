d6079932a52e0e07af3aa0a8ca2ed79a2dc2b80f
Roshitha <roshithaignatius21@gmail.com>
2021-07-16 23:45:57 +0530

    Add sample pages

--- error-404.php
+++ error-404.php
@@ -1,46 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Error 404 | Syndeo</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no">
-    <meta name="title" content="Syndeo">
-    <?php include "components/css-assets.php"; ?>
-</head>
+  <head>
+    <?php include "components/css-assets.php" ?>
+    <title>404 error | Syndeo</title>
+  </head>
 
-<body>
-    <?php include "components/header.php"; ?>
-    <main>
-        <section class="min-vh-100 d-flex align-items-center justify-content-center">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 text-center text-dark d-flex align-items-center justify-content-center">
-                        <div>
-                            <a href="index.php">
-                                <img class="img-fluid w-50" src="assets/img/illustrations/404.svg" alt="girl illustration">
-                            </a>
-                            <h1 class="mt-4">404 Page
-                                <span class="font-weight-bolder text-primary">
-                                    not found
-                                </span>
-                            </h1>
-                            <p class="lead my-4 px-lg-11 font-weight-bold">
-                                Oops! Looks like you followed a bad link. If you think this is
-                                a problem with us, please <a href="mailto:syndeo@licet.ac.in" class="font-weight-bold text-underline">tell us</a>.
-                            </p>
-                            <a class="btn btn-primary animate-hover" href="index.php">
-                                <span class="fas fa-chevron-left mr-3 pl-2 animate-left-3"></span>
-                                Go back home
-                            </a>
-                        </div>
-                    </div>
-                </div>
+  <body>
+    <!-- Error page -->
+    <div class="container min-vh-100 d-flex justify-content-center
+      align-items-center">
+      <!-- row -->
+      <div class="row">
+        <!-- col -->
+        <div class="col-12">
+          <!-- content -->
+          <div class="text-center">
+            <div class="mb-3">
+              <!-- img -->
+              <img src="assets/images/error/404-error-img.png" alt=""
+                class="img-fluid">
             </div>
-        </section>
-    </main>
-    <?php include "components/footer.php"; ?>
-    <?php include "components/js-assets.php"; ?>
-</body>
+            <!-- text -->
+            <h1 class="display-4 fw-bold">Oops! the page not found.</h1>
+            <p class="mb-4">Or simply leverage the expertise of our consultation
+              team.</p>
+              <!-- button -->
+            <a href="index.php" class="btn btn-primary">Go Home</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Scripts -->
+    <?php include "components/js-assets.php" ?>
+  </body>
 
 </html>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <?php include "components/css-assets.php" ?>
+    <title>Syndeo App</title>
+  </head>
+
+  <body>
+    <div id="db-wrapper">
+      <!-- navbar vertical -->
+      <?php include "components/navbar-vertical.php" ?>
+       <!-- Page content -->
+      <div id="page-content">
+      <?php include "components/header.php" ?>
+        <!-- Container fluid -->
+        <div class="bg-info pt-10 pb-21"></div>
+        <div class="container-fluid mt-n22 px-6">
+          <div class="row">
+            <div class="col-lg-12 col-md-12 col-12">
+              <!-- Page header -->
+              <div>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="mb-2 mb-lg-0">
+                    <h3 class="mb-0 fw-bold text-white">Title</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- row  -->
+          <div class="row mt-6">
+            <div class="col-md-12 col-12">
+              <!-- card  -->
+              <div class="card">
+                <!-- card header  -->
+                <div class="card-header bg-white border-bottom-0 py-4">
+                  <h4 class="mb-0">Title</h4>
+                </div>
+
+                <div class="card-body text-center">
+                  
+                    <!-- Put your content here --> 
+                    Content
+
+                </div>
+                
+                <!-- card footer  -->
+                <div class="card-footer bg-white border-white text-end">
+                  <a href="#" class="btn btn-primary">Button</a>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+
+    <!-- Scripts -->
+    <?php include "components/js-assets.php" ?>
+
+
+  </body>
+
+</html>
