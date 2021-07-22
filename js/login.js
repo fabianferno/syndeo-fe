7871cd5fe33fda3e2ebb95d82b2fd4de2f7e8f97
Roshitha <roshithaignatius21@gmail.com>
2021-07-23 01:21:44 +0530

    sign in -> login

--- js/login.js
+++ js/login.js
@@ -1,127 +1,42 @@
-$(document).ready(function () {
-  // Check if user is also signed in
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      // User is signed in.
-      firebase
-        .auth()
-        .currentUser.getIdToken(true)
-        .then(function (idToken) {
-          window.id_token = idToken;
-          window.uid = user.uid;
-          localStorage.profilePic = user.photoURL;
+$(document).ready(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          window.location.href = "home.php"
+        } else {
+            // signed out
+            document.getElementById('pageLoader').classList.add('d-none');
+            document.getElementById('pageContent').classList.remove('d-none');
+        }
+    });
+    
+    document.getElementById('signInForm').addEventListener('submit', (e) => {
+        e.preventDefault();
 
-          console.log(uid);
-          console.log(idToken);
+        document.getElementById('signInButton').setAttribute('disabled', 'disabled');
+        document.getElementById('signInButtonText').classList.add('d-none');
+        document.getElementById('signInButtonLoader').classList.remove('d-none');
 
-          // Check if registered with backend
-          $.ajax({
-            type: "POST",
-            url: APIRoute + "users/register",
-            datatype: "html",
-            data: {
-              uid: uid,
-              idToken: id_token,
-            },
-            success: function (response) {
-              console.log(response);
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
 
-              // Redirect if admin
-              if (response == "admin") {
-                firebase
-                  .auth()
-                  .signOut()
-                  .then(() => {
-                    window.location.replace(adminAppUrl);
-                  });
-              }
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in
+                window.user = userCredential.user;
 
-              // If the profile is incomplete
-              else if (response == "profile-incomplete") {
-                window.location.replace("complete-profile.php");
-              }
+                localStorage.uid = userCredential.user.uid;
+                localStorage.fullName = userCredential.user.displayName;
+                localStorage.email = userCredential.user.email;
+                
+                console.log(localStorage.uid);
+                window.location.href = "home.php";
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
 
-              // if the profile is complete
-              else if (response == "exists") {
-                window.location.replace("dashboard.php");
-              }
+                console.log(errorMessage)
+            });
+    })
 
-              // Error !
-              else if (response == "fail") {
-                var user = firebase.auth().currentUser;
-                console.error("db-error");
-              }
-            },
-            error: function (error) {},
-          });
-        });
-    } else {
-      // No user is signed in.
-      console.log(false);
-
-      // Hide page content and show loader
-      $("#pageLoader").addClass("d-none");
-      $("#pageContent").removeClass("d-none");
-    }
-  });
-
-  // Listen for register button submit
-  $("#loginForm").submit(function (e) {
-    e.preventDefault();
-
-    // Get email and password
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    if (email == "") {
-      document.getElementById("email").classList.add("is-invalid");
-    }
-    if (password == "") {
-      document.getElementById("password").classList.add("is-invalid");
-    } else {
-      // Show loader and hide button text
-      document.getElementById("loginButtonText").classList.add("d-none");
-      document.getElementById("loginButtonLoader").classList.remove("d-none");
-      document.getElementById("loginButton").classList.add("disabled");
-
-      // Login to your account
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-          // Signed in
-          var user = userCredential.user;
-          console.log(user);
-          localStorage.profilePic = user.photoURL;
-        })
-        .catch((error) => {
-          // var errorCode = error.code;
-          var errorMessage = error.message;
-          document.getElementById("formMessage").innerHTML = errorMessage;
-          resetForm();
-        });
-    }
-  });
-});
-
-function resetForm() {
-  // Reset inputs
-  document.getElementById("email").classList.add("is-invalid");
-  document.getElementById("password").classList.add("is-invalid");
-  // Show loader and hide button text
-  document.getElementById("loginButtonText").classList.remove("d-none");
-  document.getElementById("loginButtonLoader").classList.add("d-none");
-  document.getElementById("loginButton").classList.remove("disabled");
-}
-
-function resetLoginButton() {
-  // Show loader and hide button text
-  document.getElementById("loginButtonText").classList.remove("d-none");
-  document.getElementById("loginButtonLoader").classList.add("d-none");
-  document.getElementById("loginButton").classList.add("disabled");
-}
-
-function signInWithGoogle() {
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithRedirect(provider);
-}
+})
