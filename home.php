04f375b86c15bd797887dd903dc0a1da6024de52
Fabian Ferno <fabianferno@gmail.com>
2021-08-27 19:57:54 +0530

    Home dashboard

--- home.php
+++ home.php
@@ -1,83 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
-    <?php include "components/css-assets.php" ?>
-    <title>Syndeo App</title>
-  </head>
+<head>
+  <?php include "components/css-assets.php" ?>
+  <title>Syndeo App</title>
+</head>
 
-  <body>
-    <!-- Page Loader Starts Here -->
-    <div id="pageLoader" class="container text-center d-flex align-items-center justify-content-center w-100" style="height: 100vh;">
-        <div class="spinner-border text-dark" style="width: 3rem; height: 3rem;" role="status">
-            <span class="visually-hidden">Loading...</span>
-        </div>
+<body>
+  <!-- Page Loader Starts Here -->
+  <div id="pageLoader" class="container text-center d-flex align-items-center justify-content-center w-100" style="height: 100vh;">
+    <div class="spinner-border text-dark" style="width: 3rem; height: 3rem;" role="status">
+      <span class="visually-hidden">Loading...</span>
     </div>
-    <!-- Page Loader Ends Here -->
+  </div>
+  <!-- Page Loader Ends Here -->
 
-    <div id="pageContent" class="d-none">
-      <div id="db-wrapper">
+  <div id="pageContent" class="d-none">
+    <div id="db-wrapper">
 
-        <?php include "components/navbar-vertical.php" ?>
+      <?php include "components/navbar-vertical.php" ?>
 
-        <!-- Page content -->
-        <div id="page-content">
+      <!-- Page content -->
+      <div id="page-content">
 
         <?php include "components/header.php" ?>
-        
-          <!-- Container fluid -->
-          <div class="bg-info pt-10 pb-21"></div>
-          <div class="container-fluid mt-n22 px-6">
-            <div class="row">
-              <div class="col-lg-12 col-md-12 col-12">
-                <!-- Page header -->
-                <div>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="mb-2 mb-lg-0">
-                      <h3 class="mb-0 fw-bold text-white">Home</h3>
-                    </div>
+
+        <!-- Container fluid -->
+        <div class="bg-info pt-10 pb-21"></div>
+        <div class="container-fluid mt-n22 px-6">
+          <div class="row">
+            <div class="col-lg-12 col-md-12 col-12">
+              <!-- Page header -->
+              <div>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="mb-2 mb-lg-0">
+                    <h3 class="mb-0 fw-bold text-white">Hey, <span id="name-holder"></span>. Let's see what Syndeo has to offer.</h3>
                   </div>
                 </div>
               </div>
             </div>
-            <!-- row  -->
-            <div class="row mt-6">
-              <div class="col-md-12 col-12">
-                <!-- card  -->
-                <div class="card">
-                  <!-- card header  -->
-                  <div class="card-header bg-white border-bottom-0 py-4">
-                    <h4 class="mb-0">Home</h4>
-                  </div>
-
-                  <div class="card-body text-center">
-                    
-                      <!-- Put your content here --> 
-                      Home
-
-                  </div>
-                  
-                  <!-- card footer  -->
-                  <div class="card-footer bg-white border-white text-end">
-                    <!-- <a href="#" class="btn btn-primary">Button</a> -->
-
-                  </div>
-                </div>
-
-              </div>
-            </div>
-            
           </div>
+          <!-- row  -->
+          <div class="row mt-6">
+            <div class="col-md-12 col-12">
+              <!-- card  -->
+              <div class="card card-body my-2">
+                <h4 class="mb-2 w-50">A perfect profile will get you the most perfect connections.</h4>
+                <!-- card footer  -->
+                <div class="card-footer bg-white border-white text-end">
+                  <a href="edit-profile.php" class="btn btn-primary">Edit your profile now.</a>
+                </div>
+              </div>
+
+
+              <!-- card  -->
+              <div class="card card-body my-2">
+                <h4 class="mb-2 w-50">Find yourself a Mentor who matches your interests! Build strong connections with the industry experts.</h4>
+                <!-- card footer  -->
+                <div class="card-footer bg-white border-white text-end">
+                  <a href="search-profile.php" class="btn btn-primary">Search for Mentors</a>
+                </div>
+              </div>
+
+              <!-- card  -->
+              <div class="card card-body my-2">
+                <h4 class="mb-2 w-50">Manage your Password and change your preferences for this mentorship program.</h4>
+                <!-- card footer  -->
+                <div class="card-footer bg-white border-white text-end">
+                  <a href="settings.php" class="btn btn-primary">Go to Settings</a>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
+  </div>
 
-    <!-- Scripts -->
-    <?php include "components/js-assets.php" ?>
-    <script src="js/auth-status.js"></script>
-    <script src="js/home.js"></script>
+  <!-- Scripts -->
+  <?php include "components/js-assets.php" ?>
+  <script src="js/auth-status.js"></script>
+  <script src="js/home.js"></script>
 
 
-  </body>
+</body>
 
 </html>
--- js/home.js
+++ js/home.js
@@ -1,5 +1,72 @@
-function pageScript () {
-  document.getElementById('pageLoader').classList.add('d-none');
-  document.getElementById('pageContent').classList.remove('d-none');
+function pageScript() {
+  $.ajax({
+    type: "GET",
+    url: APIRoute + "users?uid=" + window.uid,
+    datatype: "html",
+
+    success: function (response) {
+      // if (response.profilePic == null)
+      //   document.getElementById("profileAvatar").src =
+      //     "assets/images/png/avatar.jpg";
+      // else document.getElementById("profileAvatar").src = response.profilePic;
+      document.getElementById("name-holder").innerHTML = response.fullName;
+      //$(".designation").html(response.designation);
+
+      // document.getElementById("summary").innerHTML = response.summary;
+      // document.getElementById("email").innerHTML = response.email;
+      // document.getElementById("country").innerHTML = response.country;
+      // document.getElementById("linkedInUrl").innerHTML = response.linkedInURL;
+      // document.getElementById("portfolioLink").innerHTML = response.resumeLink;
+      // document.getElementById("branch").innerHTML = response.department;
+      // document.getElementById("year").innerHTML = response.batch;
+      // document.getElementById("areasOfInterest").innerHTML =
+      //   response.areasOfInterest;
+      // document.getElementById("languages").innerHTML = response.languages;
+
+      // if (localStorage.type == "Student" && response.isMentor == 1) {
+      //   document.getElementById("askMentorshipBtn").classList.remove("d-none");
+      //   document
+      //     .getElementById("askMentorshipBtn")
+      //     .setAttribute("data-uid", response.uid);
+      // }
+
+      // if (response.isMentor == 1) {
+      //   document.getElementById("mentorsInfo").classList.remove("d-none");
+      //   var elem = document.createElement("p");
+      //   elem.innerHTML = "None";
+
+      //   if (response.higherEd == null)
+      //     document.getElementById("higherEd").appendChild(elem.cloneNode(true));
+      //   else {
+      //     response.higherEd.split(",").forEach((ed) => {
+      //       var ele = document.createElement("p");
+      //       ele.innerHTML = ed;
+      //       document.getElementById("higherEd").appendChild(ele);
+      //     });
+      //   }
+      //   if (response.licensesAndCerts == null)
+      //     document.getElementById("certs").appendChild(elem.cloneNode(true));
+      //   else {
+      //     response.licensesAndCerts.split(",").forEach((ed) => {
+      //       var ele = document.createElement("p");
+      //       ele.innerHTML = ed;
+      //       document.getElementById("certs").appendChild(ele);
+      //     });
+      //   }
+      // }
+
+      // if (localStorage.type == "Mentor" && response.isMentor == 0) {
+      //   var allocationId = url.searchParams.get("allocationId");
+      //   document
+      //     .getElementById("acceptMentorshipRequest")
+      //     .classList.remove("d-none");
+      //   document
+      //     .getElementById("askMentorshipBtn")
+      //     .getAttribute("data-allocationId", allocationId);
+      // }
+      document.getElementById("pageLoader").classList.add("d-none");
+      document.getElementById("pageContent").classList.remove("d-none");
+    },
+    error: function (error) {},
+  });
 }
-   
