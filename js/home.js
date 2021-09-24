9b770096b3ed31dab83f5673b6ecd63a3ae50491
Fabian Ferno <fabianferno@gmail.com>
2021-09-24 10:46:07 +0530

    Mentee variable

--- js/home.js
+++ js/home.js
@@ -12,15 +12,18 @@ function pageScript() {
     url: APIRoute + "allocations?uid=" + window.uid,
     datatype: "html",
     success: function (response) {
+      console.log(response);
       var menteeCards = "";
-      response.forEach((student) => {
-        console.log(student);
+      response.forEach((mentee) => {
+        console.log(mentee);
         var htmlContent =
           '<div class="col-md-6 col-12"><div class="card card-body my-2"><h4 class="mb-2 w-50">' +
-          student.name +
+          mentee.name +
           "</h4><p>" +
-          student.summary +
-          '</p><div class="card-footer bg-white border-white text-end"><a href="edit-profile.php" class="btn btn-primary">View Profile</a></div></div></div>';
+          mentee.summary +
+          '</p><div class="card-footer bg-white border-white text-end"><a href="profile.php?uid=' +
+          mentee.uid +
+          '" class="btn btn-primary">View Profile</a></div></div></div>';
 
         menteeCards = menteeCards + htmlContent;
       });
